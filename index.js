a411505808a6574599814ebdd347369cfaad8804
Mikola Lysenko <mikolalysenko@gmail.com>
2013-02-10 12:10:49 -0800

    Tons of bug fixes

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 "use strict";
 
 var EPSILON = 1e-6;
-
 var numeric = require("numeric");
 var bits = require("bit-twiddle");
+
+function BoundaryPoint(coord, cells) {
+  this.coord = coord;
+  this.cells = cells;
+}
 
 function Grid(cells, positions, tolerance, grid) {
   this.cells = cells;
@@ -23,22 +27,18 @@ function index(x) {
     case 3:
       return bits.interleave3(x[0], x[1], x[2]);
     default:
-      return -1;
+      return x.join("|");
   };
 }
 
-
-// (a0 * p0 + a1 * q0  - x0)^2 + (a0 * p1 + a1 * q1 - x1)^2
-
-// a0*a0 *p0 *p0 + 2*a0*a1*p0*q0 + a1*a1*q0*q0-a0 * p0*x0 - a1*q0*x0
-
-//Computes distance from x to cell c
+//Computes distance from x to cell c using quadratic programming.
+//This works in n-dimensions, but could probably be optimized using the GJK algorithm
 function cellDistance(c, positions, x) {
   var D = numeric.rep([c.length, c.length], 0.0);
   var dvec = numeric.rep([c.length], 0.0);
   for(var i=0; i<c.length; ++i) {
     var pi = positions[c[i]];
-    dvec[i] = 2.0 * numeric.dot(pi, x);
+    dvec[i] = numeric.dot(pi, x);
     for(var j=0; j<c.length; ++j) {
       var pj = positions[c[j]];
       D[i][j] = D[j][i] = numeric.dot(pi, pj);
@@ -46,8 +46,8 @@ function cellDistance(c, positions, x) {
   }
   var A = numeric.rep([c.length, c.length+2], 0.0);
   var b = numeric.rep([c.length+2], 0.0);
-  b[0] = 1.0;
-  b[1] = -1.0;
+  b[0] = 1.0-EPSILON;
+  b[1] = -(1.0+EPSILON);
   for(var i=0; i<c.length; ++i) {
     A[i][0]   = 1;
     A[i][1]   = -1
@@ -56,23 +56,17 @@ function cellDistance(c, positions, x) {
   return numeric.solveQP(D, dvec, A, b);
 }
 
-//Locates the closest cell within tolerance to x
-Grid.prototype.closestCells = function(x) {
-  var tolerance = this.tolerance;
-  var positions = this.positions;
-  var ix = new Array(x.length);
-  for(var i=0; i<x.length; ++i) {
-    ix[i] = (x[0] / tolerance) | 0;
-  }
-  var nbhd = this.grid[index(ix)];
-  if(!nbhd) {
-    return null;
-  }
+
+BoundaryPoint.prototype.closestCells = function(grid, x) {
+  var nbhd = this.cells;
   var d = Number.POSITIVE_INFINITY;
   var r = [];
   for(var i=0; i<nbhd.length; ++i) {
     var c = nbhd[i];
-    var t = cellDistance(this.cells[c], positions, x);
+    var t = cellDistance(grid.cells[c], grid.positions, x);
+    if(t.message.length > 0 || isNaN(t.value[0])) {
+      continue;
+    }
     t.cell = c;
     if(Math.abs(t.value[0] - d) < EPSILON) {
       r.push(t);
@@ -85,10 +79,10 @@ Grid.prototype.closestCells = function(x) {
   var points = [];
   for(var k=0; k<r.length; ++k) {
     var point = numeric.rep([x.length], 0.0);
-    var cell = this.cells[r[k].cell];
+    var cell = grid.cells[r[k].cell];
     var solution = r[k].solution;
     for(var i=0; i<cell.length; ++i) {
-      var p = positions[cell[i]];
+      var p = grid.positions[cell[i]];
       var w = solution[i];
       for(var j=0; j<x.length; ++j) {
         point[j] += p[j] * w;
@@ -100,24 +94,31 @@ Grid.prototype.closestCells = function(x) {
   return {
     points: points,
     cells: cells,
-    distance: numeric.norm2(numeric.sub(x, points[0]))
+    distance: Math.sqrt(2.0*r[0].value[0] + numeric.dot(x,x))
   };
 }
 
-function fill0(grid, lo, hi, c) {
-  if(grid[0]) {
-    grid[0].push(c);
-  } else {
-    grid[0] = [c];
+//Locates the closest cell within tolerance to x
+Grid.prototype.closestCells = function(x) {
+  var tolerance = this.tolerance;
+  var ix = new Array(x.length);
+  for(var i=0; i<x.length; ++i) {
+    ix[i] = Math.floor(x[i] / tolerance);
   }
+  var nbhd = this.grid[index(ix)];
+  if(!nbhd) {
+    return null;
+  }
+  return nbhd.closestCells(this, x);
 }
 
+//Optimized stuff to fill in grid
 function fill1(grid, lo, hi, c) {
   for(var i=lo[0]; i<=hi[0]; ++i) {
     if(grid[i]) {
-      grid[i].push(c);
+      grid[i].cells.push(c);
     } else {
-      grid[i] = [c];
+      grid[i] = new BoundaryPoint([i], [c]);
     }
   }
 }
@@ -132,15 +133,25 @@ function expand2(x) {
 }
 
 function fill2(grid, lo, hi, c) {
+  var scoord = [0,0];
+  var tol = 2 * grid.tolerance * grid.tolerance;
   for(var i=lo[1]; i<=hi[1]; ++i) {
     var x = expand2(i)<<1;
+    scoord[1] = i * grid.tolerance;
     for(var j=lo[0]; j<=hi[0]; ++j) {
+      scoord[0] = y * grid.tolerance;
+      var cdist = cellDistance(grid.cells[c], grid.positions, scoord);
+      if(cdist.message.length > 0 ||
+         isNaN(cdist.value[0]) ||
+          (numeric.dot(scoord, scoord) + 2.0 * cdist.value[0]) >= tol) {
+          continue;
+      }
       var y = x + expand2(j);
-      var g = grid[y];
+      var g = grid.grid[y];
       if(g) {
-        g.push(c);
+        g.cells.push(c);
       } else {
-        grid[y] = [c];
+        grid.grid[y] = new BoundaryPoint([j,i], [c]);
       }
     }
   }
@@ -156,31 +167,83 @@ function expand3(x) {
 }
 
 function fill3(grid, lo, hi, c) {
+  var scoord = [0,0,0];
+  var tol = 3.0 * grid.tolerance * grid.tolerance;
   for(var i=lo[2]; i<=hi[2]; ++i) {
     var x = expand3(i)<<2;
+    scoord[2] = grid.tolerance * i;
     for(var j=lo[1]; j<=hi[1]; ++j) {
       var y = x + (expand3(j) << 1);
+      scoord[1] = grid.tolerance * j;
       for(var k=lo[0]; k<=hi[0]; ++k) {
+        scoord[0] = grid.tolerance * k;
+        var cdist = cellDistance(grid.cells[c], grid.positions, scoord);
+        if( cdist.message.length > 0 ||
+            isNaN(cdist.value[0]) ||
+            (numeric.dot(scoord, scoord) + 2.0 * cdist.value[0]) >= tol) {
+            continue;
+        }      
         var z = y + expand3(k);
-        var g = grid[z];
+        var g = grid.grid[z];
         if(g) {
-          g.push(c);
+          g.cells.push(c);
         } else {
-          grid[z] = [c];
+          grid.grid[z] = new BoundaryPoint([k,j,i], [c]);;
         }
       }
     }
   }
 }
 
-function filln(grid, lo, hi, c) {
-  throw Error("Not implemented!");
+function fill_rec(grid, lo, hi, c, n, coord) {
+  if(n < 0) {
+    var scoord = numeric.mul(grid.tolerance, coord);
+    var cdist = cellDistance(grid.cells[c], grid.positions, scoord);
+    if(cdist.message.length > 0 ||
+       isNaN(cdist.value[0]) ||
+        (numeric.dot(scoord, scoord) + 2.0 * cdist.value[0]) >= (lo.length * grid.tolerance *grid.tolerance) ) {
+        return;
+    }
+    var k = index(coord);
+    var bnd = grid.grid[k];
+    if(bnd) {
+      bnd.cells.push(c);
+    } else {
+      grid.grid[k] = new BoundaryPoint(coord.slice(0), [c])
+    }
+  } else {
+    for(coord[n]=lo[n]; coord[n]<=hi[n]; ++coord[n]) {
+      fill_rec(grid, lo, hi, c, n-1, coord);
+    }
+  }
+}
+
+function fill(grid, lo, hi, c) {
+  var n = lo.length;
+  switch(n) {
+    case 0:
+      //Do nothing
+    break;
+    case 1:
+      fill1(grid, lo, hi, c);
+    break;
+    case 2:
+      fill2(grid, lo, hi, c);
+    break;
+    case 3:
+      fill3(grid, lo, hi, c);
+    break;
+    default:
+      fill_rec(grid, lo, hi, c, n-1, new Array(n));
+    break;
+  }
 }
 
 function createGrid(mesh, tolerance) {
   var cells = mesh.cells || mesh.faces;
   var positions = mesh.positions;
-  var grid = {};
+  var grid = {};  
+  var result = new Grid(cells, positions, tolerance, grid);
   var d = positions[0].length;
   var lo = new Array(d);
   var ilo = new Array(d);
@@ -201,26 +264,10 @@ function createGrid(mesh, tolerance) {
     }
     for(var j=0; j<d; ++j) {
       ilo[j] = Math.floor(lo[j] / tolerance) - 1;
-      ihi[j] = Math.floor(hi[j] / tolerance) + 1;
+      ihi[j] = Math.ceil(hi[j] / tolerance) + 1;
     }
-    switch(d) {
-      case 0:
-        fill0(grid, ilo, ihi, i)
-      break;
-      case 1:
-        fill1(grid, ilo, ihi, i);
-      break;
-      case 2:
-        fill2(grid, ilo, ihi, i);
-      break;
-      case 3:
-        fill3(grid, ilo, ihi, i);
-      break;
-      default:
-        filln(grid, ilo, ihi, i);
-      break;
-    }
+    fill(result, ilo, ihi, i);
   }
-  return new Grid(cells, positions, tolerance, grid);
+  return result;
 }
 module.exports = createGrid;
--- test/test.js
+++ test/test.js
@@ -2,5 +2,5 @@ var mesh = require("conway-hart")("C");
 var grid = require("../index.js")(mesh, 0.5);
 console.log(grid);
 
-console.log(grid.closestCell([ 1, 0, 0]));
-console.log(grid.closestCell([ 1, 1, 1]));
+console.log(grid.closestCells([ 1, 0, 0]));
+console.log(grid.closestCells([ 1, 1, 1]));
